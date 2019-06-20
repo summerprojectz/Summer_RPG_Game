cca6699f38fe8f1f7590d1790acc3fdb8bd38aa1
Calum Mathison <40406464@live.napier.ac.uk>
2019-06-20 12:20:33 +0100

    Added Cogs
    Added Cog model, cog material and basic rotation script

---
+++ SummerGame/Assets/Scripts/TempGearAnim.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempGearAnim : MonoBehaviour
+{
+    public int direction = 1;
+    public float speed = 2f;
+
+    void Update()
+    {
+        transform.Rotate(0, speed * direction, 0);
+    }
+}
