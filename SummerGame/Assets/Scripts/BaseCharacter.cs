a32697c03fd594763b4db405e0a5b49fdec9970a
Calum Mathison <40406464@live.napier.ac.uk>
2019-06-20 17:34:19 +0100

    Added Base Char

---
+++ SummerGame/Assets/Scripts/BaseCharacter.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BaseCharacter : MonoBehaviour
+{
+    #region Fields
+    Dictionary<string, int> _characteristics;
+    #endregion
+
+    #region Properties
+    #endregion
+
+    #region Methods
+    public virtual void Awake()
+    {
+        _characteristics.Add("WeaponSkill", 1);
+        _characteristics.Add("BalisticSkill", 1);
+        _characteristics.Add("Strength", 1);
+        _characteristics.Add("Toughness", 1);
+        _characteristics.Add("Agility", 1);
+        _characteristics.Add("Intellegence", 1);
+        _characteristics.Add("Perception", 1);
+        _characteristics.Add("WillPower", 1);
+        _characteristics.Add("Fellowship", 1);
+
+    }
+    #endregion
+}
