5070669576009519ead5aeec840dfcac8452edff
Calum Mathison <40406464@live.napier.ac.uk>
2019-06-20 17:14:44 +0100

    Added Standard Assets

--- SummerGame/Assets/Scripts/Combat/CombatManager.cs
+++ SummerGame/Assets/Scripts/Combat/CombatManager.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class CombatManager : MonoBehaviour
 {
+    enum CombatState
+    {
+        Selection,
+        Playout
+    }
+
+    CombatState _currentState;
+
+    bool _inCombat;
+    
+    //Holds all combatents including player
+    List<GameObject> _combatents;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _currentState = CombatState.Selection;
+        _inCombat = true;
+        _combatents = new List<GameObject>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        while (_inCombat == true)
+        {
+            switch (_currentState)
+            {
+                case CombatState.Selection:
+                    break;
+                case CombatState.Playout:
+                    break;
+            }
+        }
     }
 }
